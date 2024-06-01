627811283640813b8453578d87cb2d00825ade34
Redho Arifin <redhoarifin2017@gmail.com>
2024-06-02 01:45:53 +0900

    add loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+import { Skeleton } from "@/components/ui/skeleton";
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <Skeleton className="h-96 w-96"/>;
+}
